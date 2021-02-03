f1a499d9ed7c7bbdff48d22a620392e7d97791aa
Aniruddh <ojhaa94@gmail.com>
2021-02-03 11:51:29 +0000

    refactors code to prevent repeating

--- writing_syntax/spec/string_analyzer_spec.rb
+++ writing_syntax/spec/string_analyzer_spec.rb
@@ -1,34 +1,33 @@
 require 'string_analyzer'
 
 describe StringAnalyzer do
+sa = StringAnalyzer.new
+# just refactored so that i don't have to repeat the above line
+# good to know i can just reuse this variable
   context "With valid input" do
 
     it "should detect when a string contains vowels" do
-      sa = StringAnalyzer.new
-      # creating a variable for rspec to use
       test_string = 'uuu'
       # creates a variable which can be reassigned at every new it statement
       expect(sa.has_vowels? test_string).to be true
       # passes test_string as an argument into the has_vowels? method
     end 
 
-    it "should detect when a string doesn't contain vowels" do
-      sa = StringAnalyzer.new
+    it "should detect when a string doesn't contain vowels" do   
       test_string = 'bcdfg'
       expect(sa.has_vowels? test_string).to be false
     end
 
     # I'm now going to make a few more it statements to practice.
-    it "should detect when one vowel between consonants" do 
-      sa = StringAnalyzer.new
+    it "should detect when one vowel between consonants" do      
       test_string = 'car'
       expect(sa.has_vowels? test_string).to be true 
     end 
 
-    it "should return false if string is empty" do
-      sa = StringAnalyzer.new
+    it "should return false if string is empty" do   
       test_string = ''
       expect(sa.has_vowels? test_string).to be false
     end 
+    
   end
 end
