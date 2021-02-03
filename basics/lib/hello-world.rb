d41a9957a4c0bc483c8d2349b5cb42d09084a10e
Aniruddh <ojhaa94@gmail.com>
2021-02-03 11:12:15 +0000

    inline comments to explain the basic syntax

--- basics/lib/hello-world.rb
+++ basics/lib/hello-world.rb
@@ -8,13 +8,22 @@ class HelloWorld
 end 
 
 describe HelloWorld do
+	# the describe keyword takes either a string or class argument
+	# the purpose is to define an "Example Group"
 	context "When testing the HelloWorld class" do
-
+		# the context keyword can take a string or class argument
+		# the purpose is to enclose tests of a certain type together.
 		it "should say 'Hello World' when we call the say_hello method" do
+			# the it keyword can take a string or class argument
+			# the purpose is to describe a specific behaviour
 			hw = HelloWorld.new
 			message = hw.say_hello
 			expect(message).to eq "Hello World!"
+			# the expect keyword is used to define an expectation
+			# the to/not_to keyword is used with expect
+			# eq is a matcher keyword. more on that later.
 		end
 	
 	end
 end 
+
